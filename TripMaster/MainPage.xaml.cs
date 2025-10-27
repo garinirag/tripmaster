5ad7950dfe3533429bd019205aa435037825e0d2
Antoine GARIN <antoine.garin@gmail.com>
2025-10-28 00:09:15 +0100

    Bouton stop : popup de confirmation

--- TripMaster/MainPage.xaml.cs
+++ TripMaster/MainPage.xaml.cs
@@ -4,15 +4,24 @@ namespace TripMaster;
 
 public partial class MainPage : ContentPage
 {
+    private readonly MainPageViewModel vm;
+
     public MainPage(MainPageViewModel vm)
     {
         InitializeComponent();
         BindingContext = vm;
+        this.vm = vm;
     }
 
     protected override void OnAppearing()
     {
         base.OnAppearing();
         DeviceDisplay.Current.KeepScreenOn = true;
+    }
+
+    protected async void StopButton_Clicked(object sender, EventArgs e)
+    {
+        if (await DisplayAlert(@"/!\ ARRÊTER LA SESSION ? /!\", "", "Confirmer", "Annuler"))
+            vm.StopCommand.Execute(null);
     }
 }
